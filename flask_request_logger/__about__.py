5496ed78b4237fb3483a177707a4f25194cdb64d
Bobson Lin <bobson801104@gmail.com>
2018-11-07 11:21:35 +0800

    build(setup.py): Update setup.py informations / Update .gitignore

---
+++ flask_request_logger/__about__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0'
+__author__ = 'Bobson Lin'
+__email__ = 'bobson801104@gmail.com'
+__description__ = 'A Flask extension for recording requests and responses'
--- setup.py
+++ setup.py
@@ -4,13 +4,37 @@ from os.path import join, dirname
 with open(join(dirname(__file__), 'requirements.txt'), 'r') as f:
     install_requires = f.read().split("\n")
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+exec(compile(open('flask_request_logger/__about__.py').read(), 'flask_request_logger/__about__.py', 'exec'))
+
 setup(
     name='flask-request-logger',
-    version='0.1.0',
+    version=__version__,
+    description=__description__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/BbsonLin/flask-request-logger',
     license='MIT',
-    author='Bobson Lin',
-    author_email='bobson801104@gmail.com',
+    author=__author__,
+    author_email=__email__,
     packages=['flask_request_logger'],
-    install_requires=install_requires
+    install_requires=install_requires,
+    zip_safe=False,
+    keywords='flask filer filesystem',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
