f9be6fc4366aa967a1f19fe684b5eaaa5bd601b3
Bobson Lin <bobson801104@gmail.com>
2019-04-23 14:45:17 +0800

    chore: Update .travis.yml / bump version 0.3.1

--- flask_request_logger/__about__.py
+++ flask_request_logger/__about__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Bobson Lin'
 __email__ = 'bobson801104@gmail.com'
 __description__ = 'A Flask extension for recording requests and responses'
